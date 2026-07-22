30910ec1e8060b9979239645d1346f2d81df40c4
samuelola <sammyola122@gmail.com>
2026-07-22 17:01:37 +0100

    fourth commit

--- notes-service/Modules/Notes/Application/QueryHandlers/GetUsersNoteHandler.php
+++ notes-service/Modules/Notes/Application/QueryHandlers/GetUsersNoteHandler.php
@@ -57,7 +57,7 @@ class GetUsersNoteHandler
                                 'from' => $paginator->firstItem(),
                                 'to' => $paginator->lastItem(),
                                 'has_more_pages' => $paginator->hasMorePages(),
-                        ],
+                            ],
                         ];
                     }
                 );
