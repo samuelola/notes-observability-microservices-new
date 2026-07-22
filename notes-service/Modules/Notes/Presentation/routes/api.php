3e8547f9285facd3a32e0c633f87f20a2d1bf145
samuelola <sammyola122@gmail.com>
2026-07-22 17:23:51 +0100

    deploy new fix

--- notes-service/Modules/Notes/Presentation/routes/api.php
+++ notes-service/Modules/Notes/Presentation/routes/api.php
@@ -5,15 +5,19 @@ use Modules\Notes\Presentation\Http\Controllers\Api\V1\NoteController;
 
 // PROTECTED ROUTES
 
-   Route::prefix('api/v1')->group(function () {
-
+Route::prefix('api/v1')->group(function () {
     // Route::get('/metrics', [NoteController::class, 'metrics']);
-    Route::middleware(['auth.service', 'note_inactive_owner', 'note.correlation', 'note.tracing'])->group(function () {
+
+    Route::middleware([
+        'auth.service',
+        'note_inactive_owner',
+        'note.correlation',
+        'note.tracing',
+    ])->group(function () {
         Route::get('/notes', [NoteController::class, 'index']);
         Route::post('/notes', [NoteController::class, 'store']);
         Route::get('/notes/{id}', [NoteController::class, 'show']);
         Route::put('/notes/{id}', [NoteController::class, 'update']);
         Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
-
     });
 });
